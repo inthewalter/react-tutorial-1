735396aa25511697ae1a96f2705149829ae5381d
inthewalter <21400564@handong.edu>
2019-06-07 14:54:10 +0900

    step 01. initial project
    - create component Main, Viewr, WebtoonHome

---
+++ src/container/Viewer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Viewer extends Component {
+  render() {
+    return (
+      <div>
+        Viewer
+      </div>
+    )
+  }
+}
---
+++ src/container/WebtoonHome.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class WebtoonHome extends Component {
+  render() {
+    return (
+      <div>
+        WebtoonHome
+      </div>
+    )
+  }
+}
