095af23d2d8ee90e0f3f1a88208e3b54302e44c0
inthewalter <21400564@handong.edu>
2019-06-08 02:18:37 +0900

    step 06. add viewer

--- src/container/Viewer.js
+++ src/container/Viewer.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react'
-
-import Header from "../component/Header";
-import Gnb from "../component/Gnb";
-import Footer from "../component/Footer";
-
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class Viewer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      episodeId: parseInt(props.match.params.episodeId, 10),
+      episode: {}
+    }
+  }
+
+  componentDidMount() {
+    this._getEpisodeList();
+  }
+
+  _getEpisodeList() {
+    const apiUrl = '/dummy/episode_list.json';
+
+    axios.get(apiUrl)
+      .then(data => {
+        this.setState({
+          episode: data.data.webtoonEpisodes.find(episode => (
+            episode.id === this.state.episodeId
+          ))
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
   render() {
+    const episode = this.state.episode;
+
     return (
-      <div>
-        <Header />
-        <Gnb />
-        Viewer
-        <Footer />
+      <div className="wrap_viewer">
+      {
+        episode.id
+        ? (
+          <div>
+            <div className="top_viewer">
+              { episode.title }
+              <Link to={`/webtoon/${ episode.webtoonId }`} className="btn_close">X</Link>
+            </div>
+            <div className="wrap_images">
+              { episode.images.map((img, index) => (
+                <img key={ index } src={ img } />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <span>Loading...</span>
+        )
+      }
       </div>
     )
   }
