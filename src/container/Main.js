5205ac3db7bce6cd2a40fdad43cc2c2ef41461df
inthewalter <21400564@handong.edu>
2019-06-08 01:28:54 +0900

    step 04. show the dummy info

--- src/container/Main.js
+++ src/container/Main.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 import Header from "../component/Header";
 import Gnb from "../component/Gnb";
 import Footer from "../component/Footer";
-
+import WebtoonList from "../component/WebtoonList";
 
 export default class Main extends Component {
   render() {
@@ -11,9 +12,49 @@ export default class Main extends Component {
       <div>
         <Header />
         <Gnb />
-        Main
+        { this.state.webtoonList.length > 0
+          ? (
+            <WebtoonList list={
+              this.state.webtoonList.filter(webtoon => (
+                webtoon.day === this.state.day
+              ))
+              } />
+          )
+          : (
+            <span>
+              Loading...
+            </span>
+          )
+        }
         <Footer />
       </div>
     )
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      day: 'mon',
+      webtoonList: []
+    }
+  }
+
+  componentDidMount() {
+    this._getList();
+  }
+
+  _getList() {
+    const apiUrl = 'dummy/webtoon_list.json';
+
+    axios.get(apiUrl)
+      .then(data => {
+        this.setState({
+          webtoonList: data.data.webtoonList
+        })
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 }
